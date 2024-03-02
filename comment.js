678351b139799f54384f529c9f5f7cfd4db8eb75
Vedant Sandeep Bhosle <72183406+VedantBhosle31@users.noreply.github.com>
2024-03-02 00:04:52 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,66 @@
+// create web server
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
+
+function templateHTML(title, list, body, control){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${control}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filelist){
+  var list = '<ul>';
+  for (var i = 0; i < filelist.length; i++) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+  }
+  list = list + '</ul>';
+  return list;
+}
+
+const app = http.createServer(function(request,response){
+    const _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            fs.readdir('./data', function(error, filelist){
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = templateList(filelist);
+                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readdir('./data', function(error, filelist){
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                    var title = queryData.id;
+                    var list = templateList(filelist);
+                    var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, 
+                    `<a href="/create">create</a> 
+                    <a href="/update?id=${title}">update</a>
+                    <form action="delete_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <input type="submit" value="delete">
+                    </form>`);
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
+        }
+    }
+}); // Added closing parenthesis and brace here
